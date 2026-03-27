da65c6cc2640c8da611f6607ac9d9591177e5bd9
evko <evgeni.kovalev@gmail.com>
2026-03-27 02:43:55 +0200

    Added deployment profiles

--- Kimppu/Services/ProductService.cs
+++ Kimppu/Services/ProductService.cs
@@ -23,55 +23,55 @@ namespace Marketplace.Services
 
 		private async Task CheckCache(bool force = false)
 		{
-			if (DateTime.UtcNow.AddSeconds(-_cacheIntervalSec).Ticks > _lastUpdateTicks || force)
-			{
-				var productConnection = _repositorySettings.CreateProductConnection();
-				_allProducts = await productConnection.QueryAsync<ProductDm>("Select * from product where removed is null order by OrderIndex");
-				_allProductRelations = await productConnection.QueryAsync<ProductRelationDm>("Select * from productRelation");
-				_lastUpdateTicks = DateTime.UtcNow.Ticks;
-			}
+			//if (DateTime.UtcNow.AddSeconds(-_cacheIntervalSec).Ticks > _lastUpdateTicks || force)
+			//{
+			//	var productConnection = _repositorySettings.CreateProductConnection();
+			//	_allProducts = await productConnection.QueryAsync<ProductDm>("Select * from product where removed is null order by OrderIndex");
+			//	_allProductRelations = await productConnection.QueryAsync<ProductRelationDm>("Select * from productRelation");
+			//	_lastUpdateTicks = DateTime.UtcNow.Ticks;
+			//}
 		}
 
 		internal async Task<IEnumerable<ProductDto>> LoadProducts(FilterDto filter)
 		{
 			await CheckCache();
 			var products = new List<ProductDto>();
-			if (filter != null)
-			{
-				if (filter.ActiveOnly == true)
-				{
-					foreach (var productEntity in _allProducts.Where(p => p.Active == true))
-					{
-						var product = new ProductDto(productEntity);
-						product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
-						product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
-						product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
-						products.Add(product);
-					}
-				}
-				else
-				{
-					foreach (var productEntity in _allProducts)
-					{
-						var product = new ProductDto(productEntity);
-						product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
-						product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
-						product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
-						products.Add(product);
-					}
-				}
-			}
-			else
-			{
-				foreach (var productEntity in _allProducts)
-				{
-					var product = new ProductDto(productEntity);
-					product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
-					product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
-					product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
-					products.Add(product);
-				}
-			}
+			////if (filter != null)
+			////{
+			////	if (filter.ActiveOnly == true)
+			////	{
+			////		foreach (var productEntity in _allProducts.Where(p => p.Active == true))
+			////		{
+			////			var product = new ProductDto(productEntity);
+			////			product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
+			////			product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
+			////			product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
+			////			products.Add(product);
+			////		}
+			////	}
+			////	else
+			////	{
+			////		foreach (var productEntity in _allProducts)
+			////		{
+			////			var product = new ProductDto(productEntity);
+			////			product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
+			////			product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
+			////			product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
+			////			products.Add(product);
+			////		}
+			////	}
+			////}
+			////else
+			////{
+			////	foreach (var productEntity in _allProducts)
+			////	{
+			////		var product = new ProductDto(productEntity);
+			////		product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
+			////		product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
+			////		product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
+			////		products.Add(product);
+			////	}
+			////}
 			return products;
 		}
 
@@ -80,17 +80,17 @@ namespace Marketplace.Services
 			await CheckCache();
 
 			ProductDto? product = null;
-			if (filter != null && !string.IsNullOrEmpty(filter.SeoLink))
-			{
-				var productEntity = _allProducts.FirstOrDefault(p => p.SeoLink == filter.SeoLink);
-				if (productEntity != null)
-				{
-					product = new ProductDto(productEntity);
-					product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
-					product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
-					product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
-				}
-			}
+			//if (filter != null && !string.IsNullOrEmpty(filter.SeoLink))
+			//{
+			//	var productEntity = _allProducts.FirstOrDefault(p => p.SeoLink == filter.SeoLink);
+			//	if (productEntity != null)
+			//	{
+			//		product = new ProductDto(productEntity);
+			//		product.Variants = GetProductRelations(product.Id, ProductRelationType.VariantProduct);
+			//		product.RelatedProducts = GetProductRelations(product.Id, ProductRelationType.RelatedProduct);
+			//		product.RelatedDesigns = GetProductRelations(product.Id, ProductRelationType.RelatedDesign);
+			//	}
+			//}
 			return product;
 		}
 
@@ -102,26 +102,26 @@ namespace Marketplace.Services
 		/// <returns></returns>
 		internal async Task AddProduct(string? currentUserID, ProductDto product)
 		{
-			var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
-			if (userHasRight && product != null)
-			{
-				var productConnection = _repositorySettings.CreateProductConnection();
-				var newProductParams = new
-				{
-					name = product.Name,
-					seoLink = product.SeoLink,
-					shortDescription = product.ShortDescription,
-					shortDescription2 = product.ShortDescription2,
-					coverImages = product.GetCoverImages(),
-					netPrice = product.NetPrice,
-					vat = product.Vat,
-					price = product.Price
-				};
-				await productConnection.ExecuteAsync(@"insert into [product] (name, seoLink, shortDescription,shortDescription2, coverImages, netPrice, vat, price, active) 
-						values (@name, @seoLink, @shortDescription, @shortDescription2, @coverImages, @netPrice, @vat, @price, 0)", newProductParams);
+			//var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
+			//if (userHasRight && product != null)
+			//{
+			//	var productConnection = _repositorySettings.CreateProductConnection();
+			//	var newProductParams = new
+			//	{
+			//		name = product.Name,
+			//		seoLink = product.SeoLink,
+			//		shortDescription = product.ShortDescription,
+			//		shortDescription2 = product.ShortDescription2,
+			//		coverImages = product.GetCoverImages(),
+			//		netPrice = product.NetPrice,
+			//		vat = product.Vat,
+			//		price = product.Price
+			//	};
+			//	await productConnection.ExecuteAsync(@"insert into [product] (name, seoLink, shortDescription,shortDescription2, coverImages, netPrice, vat, price, active) 
+			//			values (@name, @seoLink, @shortDescription, @shortDescription2, @coverImages, @netPrice, @vat, @price, 0)", newProductParams);
 
-				await CheckCache(true);
-			}
+			//	await CheckCache(true);
+			//}
 		}
 
 		/// <summary>
@@ -132,32 +132,32 @@ namespace Marketplace.Services
 		/// <returns></returns>
 		internal async Task EditProduct(string? currentUserID, ProductDto product)
 		{
-			var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
-			if (userHasRight && product != null)
-			{
-				var productEntity = _allProducts.FirstOrDefault(p => p.Id == product.Id);
-				if (productEntity != null)
-				{
-					var productConnection = _repositorySettings.CreateProductConnection();
-					var productParams = new
-					{
-						id = product.Id,
-						name = product.Name,
-						seoLink = product.SeoLink,
-						shortDescription = product.ShortDescription,
-						shortDescription2 = product.ShortDescription2,
-						coverImages = product.GetCoverImages(),
-						netPrice = product.NetPrice,
-						vat = product.Vat,
-						price = product.Price,
-						active = product.Active
-					};
-					await productConnection.ExecuteAsync(@"update [product] set name=@name, seoLink=@seoLink, shortDescription=@shortDescription, shortDescription2=@shortDescription2,
-						coverImages=@coverImages, netPrice=@netPrice, vat=@vat, price=@price, active=@active where id = @id", productParams);
+			//var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
+			//if (userHasRight && product != null)
+			//{
+			//	var productEntity = _allProducts.FirstOrDefault(p => p.Id == product.Id);
+			//	if (productEntity != null)
+			//	{
+			//		var productConnection = _repositorySettings.CreateProductConnection();
+			//		var productParams = new
+			//		{
+			//			id = product.Id,
+			//			name = product.Name,
+			//			seoLink = product.SeoLink,
+			//			shortDescription = product.ShortDescription,
+			//			shortDescription2 = product.ShortDescription2,
+			//			coverImages = product.GetCoverImages(),
+			//			netPrice = product.NetPrice,
+			//			vat = product.Vat,
+			//			price = product.Price,
+			//			active = product.Active
+			//		};
+			//		await productConnection.ExecuteAsync(@"update [product] set name=@name, seoLink=@seoLink, shortDescription=@shortDescription, shortDescription2=@shortDescription2,
+			//			coverImages=@coverImages, netPrice=@netPrice, vat=@vat, price=@price, active=@active where id = @id", productParams);
 
-					await CheckCache(true);
-				}
-			}
+			//		await CheckCache(true);
+			//	}
+			//}
 		}
 
 		/// <summary>
@@ -168,18 +168,18 @@ namespace Marketplace.Services
 		/// <returns></returns>
 		internal async Task RemoveProduct(string? currentUserID, long productId)
 		{
-			var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
-			if (userHasRight)
-			{
-				var productEntity = _allProducts.FirstOrDefault(p => p.Id == productId);
-				if (productEntity != null)
-				{
-					var productConnection = _repositorySettings.CreateProductConnection();
-					await productConnection.ExecuteAsync("update product set removed = @removed where id = @productId", new { productId = productId, removed = DateTime.UtcNow });
+			////var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
+			////if (userHasRight)
+			////{
+			////	var productEntity = _allProducts.FirstOrDefault(p => p.Id == productId);
+			////	if (productEntity != null)
+			////	{
+			////		var productConnection = _repositorySettings.CreateProductConnection();
+			////		await productConnection.ExecuteAsync("update product set removed = @removed where id = @productId", new { productId = productId, removed = DateTime.UtcNow });
 
-					await CheckCache(true);
-				}
-			}
+			////		await CheckCache(true);
+			////	}
+			////}
 		}
 
 		/// <summary>
@@ -190,23 +190,23 @@ namespace Marketplace.Services
 		/// <returns></returns>
 		internal async Task SaveProductOrder(string? currentUserID, List<int> productIds)
 		{
-			var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
-			if (userHasRight && _allProducts != null && productIds != null && productIds.Count > 0)
-			{
-				_allProducts.ToList().ForEach(p => p.OrderIndex = 0);
-				for (int i = 0; i < productIds.Count; i++)
-				{
-					var product = _allProducts.FirstOrDefault(p => p.Id == productIds[i]);
-					if (product != null)
-					{
-						product.OrderIndex = i + 1;
-					}
-				}
+			//var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
+			//if (userHasRight && _allProducts != null && productIds != null && productIds.Count > 0)
+			//{
+			//	_allProducts.ToList().ForEach(p => p.OrderIndex = 0);
+			//	for (int i = 0; i < productIds.Count; i++)
+			//	{
+			//		var product = _allProducts.FirstOrDefault(p => p.Id == productIds[i]);
+			//		if (product != null)
+			//		{
+			//			product.OrderIndex = i + 1;
+			//		}
+			//	}
 
-				var productConnection = _repositorySettings.CreateProductConnection();
-				await productConnection.ExecuteAsync(@"update product set OrderIndex = @OrderIndex where Id = @Id", _allProducts);
-				await CheckCache(true);
-			}
+			//	var productConnection = _repositorySettings.CreateProductConnection();
+			//	await productConnection.ExecuteAsync(@"update product set OrderIndex = @OrderIndex where Id = @Id", _allProducts);
+			//	await CheckCache(true);
+			//}
 		}
 
 
@@ -220,16 +220,16 @@ namespace Marketplace.Services
 		/// <returns></returns>
 		internal async Task SaveRelatedProducts(string? currentUserID, long productId, List<long> relatedProductIds, ProductRelationType relationType)
 		{
-			var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
-			if (userHasRight && _allProducts != null && productId > 0 && relatedProductIds.Count > 0)
-			{
-				var selectedRelatedProducts = _allProducts.Where(p => relatedProductIds.Contains(p.Id)).ToList();
+			//var userHasRight = await _authService.UserHasRights(currentUserID, new List<AccessRightEnum>() { AccessRightEnum.PRODUCTS });
+			//if (userHasRight && _allProducts != null && productId > 0 && relatedProductIds.Count > 0)
+			//{
+			//	var selectedRelatedProducts = _allProducts.Where(p => relatedProductIds.Contains(p.Id)).ToList();
 
-				var productConnection = _repositorySettings.CreateProductConnection();
-				await productConnection.ExecuteAsync(@"delete productRelation where productId = @Id and relationType = @relationType", new { id = productId, relationType = relationType });
-				await productConnection.ExecuteAsync(@$"insert into productRelation (productId, relationType, relatedProductId) values ({productId}, {(int)relationType}, @id)", selectedRelatedProducts);
-				await CheckCache(true);
-			}
+			//	var productConnection = _repositorySettings.CreateProductConnection();
+			//	await productConnection.ExecuteAsync(@"delete productRelation where productId = @Id and relationType = @relationType", new { id = productId, relationType = relationType });
+			//	await productConnection.ExecuteAsync(@$"insert into productRelation (productId, relationType, relatedProductId) values ({productId}, {(int)relationType}, @id)", selectedRelatedProducts);
+			//	await CheckCache(true);
+			//}
 		}
 
 		/// <summary>
@@ -241,21 +241,21 @@ namespace Marketplace.Services
 		internal List<ProductDto>? GetProductRelations(long productId, ProductRelationType relationType)
 		{
 			var productVariants = new List<ProductDto>();
-			if (productId > 0)
-			{
-				var variantIds = _allProductRelations.Where(v => v.ProductId == productId && v.RelationType == (int)relationType).Select(v => v.RelatedProductId).ToList();
-				var crossVariantIds = new List<long>();
-				if (relationType != ProductRelationType.RelatedProduct)
-				{
-					crossVariantIds = _allProductRelations.Where(v => v.RelatedProductId == productId && v.RelationType == (int)relationType).Select(v => v.ProductId).ToList();
-				}
+			////if (productId > 0)
+			////{
+			////	var variantIds = _allProductRelations.Where(v => v.ProductId == productId && v.RelationType == (int)relationType).Select(v => v.RelatedProductId).ToList();
+			////	var crossVariantIds = new List<long>();
+			////	if (relationType != ProductRelationType.RelatedProduct)
+			////	{
+			////		crossVariantIds = _allProductRelations.Where(v => v.RelatedProductId == productId && v.RelationType == (int)relationType).Select(v => v.ProductId).ToList();
+			////	}
 
-				var variantEntities = _allProducts.Where(p => variantIds.Contains(p.Id) || crossVariantIds.Contains(p.Id)).Distinct().ToList();
-				foreach (var variantEntity in variantEntities)
-				{
-					productVariants.Add(new ProductDto(variantEntity));
-				}
-			}
+			////	var variantEntities = _allProducts.Where(p => variantIds.Contains(p.Id) || crossVariantIds.Contains(p.Id)).Distinct().ToList();
+			////	foreach (var variantEntity in variantEntities)
+			////	{
+			////		productVariants.Add(new ProductDto(variantEntity));
+			////	}
+			////}
 			return productVariants;
 		}
 	}
