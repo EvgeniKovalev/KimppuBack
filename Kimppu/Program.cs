0408b54307ebae795098a2c30b9a67704e0c7626
evko <evgeni.kovalev@gmail.com>
2026-04-05 16:02:57 +0300

    changed to generate

--- Kimppu/Program.cs
+++ Kimppu/Program.cs
@@ -101,13 +101,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-else
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
 
-	app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseCors("MarketplaceCorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
